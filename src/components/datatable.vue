ae79ef5f8666e08a6eb5c5cf017f28353f792561
zhj <2411489528@qq.com>
2018-06-01 11:40:58 +0800

    1

--- src/components/datatable.vue
+++ src/components/datatable.vue
@@ -9,13 +9,33 @@
                 </el-button-group>
             </el-col>
             <el-col :span="4" :offset="1">
-                <slot name="select1"></slot>
+                <el-select v-model="current_select.current_group_code" style="width:100%" size="small" v-if="select1_code" @change="groupchange">
+                    <el-option 
+                        v-for="item in select1"
+                        :key="item[select1_code]"
+                        :label="item[select1_desc]"
+                        :value="item[select1_code]">
+                    </el-option>
+                </el-select>
             </el-col>
             <el-col :span="4" :offset="1">
-                <slot name="select2"></slot>
+                <el-select v-model="current_select.current_role_code"  style="width:100%" size="small" v-if="select2_code" @change="groupchange">
+                    <el-option
+                        v-for="item in select2"
+                        :key="item[select2_code]"
+                        :label="item[select2_desc]"
+                        :value="item[select2_code]">
+                    </el-option>
+                </el-select>
             </el-col>
             <el-col :span="7" :offset="1">
-                <slot name="search"></slot>
+                <el-input  v-model="searchcontent.inputcontent" class="input-with-select" size="small" v-if="searchfun">
+                    <el-select class="inner" v-model="searchcontent.idorname" slot="prepend">
+                        <el-option :label="searchlabel1" :value="searchvalue1"></el-option>
+                        <el-option :label="searchlabel2" :value="searchvalue2"></el-option>
+                    </el-select>
+                    <el-button slot="append" icon="el-icon-search" @click="searchpw_user"></el-button>
+                </el-input>
             </el-col>
         </el-row>
 
@@ -96,17 +116,22 @@
 <script>
 import axios from 'axios'
 export default {
-    props:['table_data','sys_tbl_clmn','getData','Createsrc','current_group_role','Editsrc','Deletesrc','U_id'],
+    /*table_data表格数据,sys_tbl_clmn配置表数据,getData获取值的Vuex的方法名,current_group_role当前搜索的条件,
+    Createsrc创建地址,Editsrc编辑地址,Deletesrc删除地址,U_id删除值的id,select1_code搜索1的值，selcet1_desc搜索1的描述(可以等于搜索1的值)
+    select2同理select1*/
+    props:['table_data','sys_tbl_clmn','getData','Createsrc','current_group_role','Editsrc','Deletesrc','U_id',
+    'select1_code','select1_desc','select2_code','select2_desc','searchlabel1','searchlabel2','searchvalue1','searchvalue2','searchfun'],
     data(){
         return{
             //选中的项
             multipleSelection:[],
-            //当前表名
+            /*
             current_tb:{
                 current_tb_name:'pw_user',
                 current_name:'',
                 current_id:''
             },
+            */
             //创建输入框
             Create_dialog:false,
             //修改输入框
@@ -115,7 +140,7 @@ export default {
             Create_form:{},
             //修改表单
             Edit_form:{},
-            //条件判断表
+            //条件判断表,根据这个表，来生成创建表单和修改表单
             condition_form:[],
             //条件判断项
             condition_form_list:{},
@@ -123,15 +148,33 @@ export default {
             condition_drop:{},
             //校验规则
             rule:{},
+            //多选框搜索1
+            select1:[],
+            //多选框搜索2
+            select2:[],
+            //多选框绑定的值
+            current_select:{
+                //多选框值1
+                current_group_code:'all',
+                //多选框值2
+                current_role_code:'all',                
+            },
+            //搜索栏里的内容，idorname是搜索的类型，inputcontent是搜索的内容
+            searchcontent:{
+                idorname:'',
+                inputcontent:''
+            },
         }
     },
     methods:{
+        //选择表格的项，用于多行表格
         handleSelectionChange(val){
             this.multipleSelection = val;
         },
+        //打开编辑框
         handleEdit(index,row){
             this.Edit_dialog = true;
-            //编辑表等于项
+            //编辑表的值等于表格该项的值
             for(var key1 in row){
                 for(var key2 in this.Edit_form){
                     if(key1 == key2){
@@ -140,14 +183,17 @@ export default {
                 }
             }
         },
+        //取消创建
         cancelCreate(formName){
             this.Create_dialog = false;
             this.resetForm(formName);
         },
+        //取消修改
         cancelEdit(formName){
             this.Edit_dialog = false;
             this.resetForm(formName);
         },
+        //单行删除
         handleDelete(index,row){
             this.$confirm('确认删除?','提示',{
                 confirmButtonText:'确定',
@@ -155,13 +201,14 @@ export default {
                 type:'warning'
             }).then(() => {
                 var _this = this;
+                //post里的this指代不同，必须用_this指向原来data的值
                 axios.post(_this.Deletesrc + row[_this.U_id])
                 .then(function(res){
                 _this.$message({
                     type:'success',
                     message:'删除成功!'
                 });
-                //刷新
+                //刷新表格
                 _this.$store.dispatch(_this.getData,_this.current_group_role);
                 })
                 .catch(function(Error){
@@ -178,6 +225,7 @@ export default {
                 });
             })
         },
+        //多行删除
         SelectDelete(){
             this.$confirm('确认删除?','提示',{
                 confirmButtonText:'确定',
@@ -390,8 +438,70 @@ export default {
                 }
                 _this.condition_form.push(list);
             });         
-        }
+        },
+        //搜索数据
+        selectdrop(){
+            //搜索输入框值预设默认值
+            this.searchcontent.idorname = this.searchvalue1;
+            //清空搜索输入框的值
+            this.searchcontent.inputcontent = '';
+            //调用前先清空select搜索值
+            this.select1 = [];
+            this.select2 = [];
+            //先生成首项值all
+            var select1list = {};
+            select1list[this.select1_code] = "all";
+            select1list[this.select1_desc] = "全部";
+            var select2list = {};
+            select2list[this.select2_code] = "all";
+            select2list[this.select2_desc] = "全部";
+            this.select1.push(select1list);
+            this.select2.push(select2list);
+            var _this = this;
+            this.table_data.forEach(function(item){
+            //a和b为判断项，当select的值与table_data值重复了，就不要添加新的项
+            var a = 0;
+            var b = 0;
+            _this.select1.forEach(function(key){
+                if(key[_this.select1_code] == item[_this.select1_code]){
+                a = 1;
+                }
+            })
+            if(a == 0){
+                var group_list = {};
+                group_list[_this.select1_code] = item[_this.select1_code];
+                group_list[_this.select1_desc] = item[_this.select1_desc];
+                //添加新的项
+                _this.select1.push(group_list);
+            }
+            _this.select2.forEach(function(key){
+                if(key[_this.select2_code] == item[_this.select2_code]){
+                a = 1;
+                }
+            })
+            if(a == 0){
+                var role_list = {};
+                role_list[_this.select2_code] = item[_this.select2_code];
+                role_list[_this.select2_desc] = item[_this.select2_desc];
+                _this.select2.push(role_list);
+            }
+            })
+        },
+        //切换select值,过滤表格
+        groupchange(){
+            this.$store.dispatch(this.getData,this.current_select); 
+        },
+        //搜索,过滤表格
+        searchpw_user(){
+            this.$store.dispatch(this.searchfun,this.searchcontent);
+        },
     }
 }
 </script>
+<style scope>
+.el-select.inner .el-input{
+  width: 100px;
+  color: #cccccc;
+}
+</style>
 
--- src/page/5kanban_report/E01.vue
+++ src/page/5kanban_report/E01.vue
@@ -10,36 +10,16 @@
       :Deletesrc="Deletesrc"
       :U_id="U_id"
       :current_group_role="current_group_role"
+      :select1_code="group_code"
+      :select1_desc="group_desc"
+      :select2_code="role_code"
+      :select2_desc="role_desc"
+      :searchlabel1="searchlabel1"
+      :searchlabel2="searchlabel2"
+      :searchvalue1="searchvalue1"
+      :searchvalue2="searchvalue2"
+      :searchfun="searchfun"
       @load="loadingdata">
-      <template slot="select1">
-        <el-select v-model="current_group_role.current_group_code" placeholder="请选择组别" style="width:100%" size="small">
-          <el-option 
-            v-for="item in searchgroups"
-            :key="item.group_code"
-            :label="item.group_desc"
-            :value="item.group_code">
-          </el-option>
-        </el-select>
-      </template>
-      <template slot="select2">
-        <el-select v-model="current_group_role.current_role_code"  placeholder="请选择用户角色" style="width:100%" size="small">
-          <el-option
-            v-for="item in searchroles"
-            :key="item.role_code"
-            :label="item.role_desc"
-            :value="item.role_code">
-          </el-option>
-        </el-select>
-      </template>
-      <template slot="search">
-        <el-input  v-model="searchcontent.inputcontent" class="input-with-select" size="small">
-          <el-select class="inner" v-model="searchcontent.idorname" slot="prepend">
-            <el-option label="登录名" value="loginid"></el-option>
-            <el-option label="用户id" value="user_id"></el-option>
-          </el-select>
-          <el-button slot="append" icon="el-icon-search" @click="searchpw_user"></el-button>
-        </el-input>
-      </template>
     </Datatable>
   </div>
 </template>
@@ -66,7 +46,7 @@ export default {
       sys_tbl_clmn:[],
       //加载动态图
       loading:true,
-      //要获取的值
+      //要获取的值,调用Vuex的方法
       getData:'get_pw_user_table',
       //创建地址
       Createsrc:"http://localhost/TRX/api/AuthPower/AddUser",
@@ -76,19 +56,16 @@ export default {
       Deletesrc:"http://localhost/TRX/api/pw_user/Delete/",
       //删除提供的id
       U_id:'user_id',
-      searchgroups:[{
-        group_code:'all',
-        group_desc:'全部'
-      }],
-      searchroles:[{
-        role_code:'all',
-        role_desc:'全部'
-      }],
-      //搜索栏里的内容，idorname是搜索的类型是ID还是登录名，inputcontent是搜索的内容
-      searchcontent:{
-        idorname:'loginid',
-        inputcontent:''
-      },
+      group_code:'group_code',
+      group_desc:'group_desc',
+      role_code:'role_code',
+      role_desc:'role_desc',
+      searchlabel1:'登录名',
+      searchlabel2:'用户id',
+      searchvalue1:'loginid',
+      searchvalue2:'user_id',
+      //调用vuex的搜索方法
+      searchfun:'searchpw_user_table'
     }
   },
   methods:{
@@ -96,39 +73,14 @@ export default {
     copysys(){
       this.sys_tbl_clmn = this.sys_tbl_clmn_data.slice(0);
     },
-    //生成select数据
-    selectdrop(){
-      var _this = this;
-      this.pw_user.forEach(function(item){
-        var a = 0;
-        var b = 0;
-        _this.searchgroups.forEach(function(key){
-          if(key.group_code == item.group_code){
-            a = 1;
-          }
-        })
-        if(a == 0){
-          var group_list = {};
-          group_list.group_code = item.group_code;
-          group_list.group_desc = item.group_desc;
-          _this.searchgroups.push(group_list);
-        }
-        _this.searchroles.forEach(function(key){
-          if(key.role_code == item.role_code){
-            a = 1;
-          }
-        })
-        if(a == 0){
-          var role_list = {};
-          role_list.role_code = item.role_code;
-          role_list.role_desc = item.role_code;
-          _this.searchroles.push(role_list);
-        }
-      })
-    },
     //加载页面内容、刷新
     loadingdata(){
+      //loading为true，页面处于待加载状态
       this.loading = true;
+      //select值还原成初始值
+      this.current_group_role.current_group_code = "all";
+      this.current_group_role.current_role_code = "all";
+      //当前表的值等于本地登录用户的值
       this.current_tb.current_name = window.sessionStorage.loginid;
       this.current_tb.current_id = window.sessionStorage.user_id;
       //获取配置表
@@ -136,26 +88,23 @@ export default {
       //获取容器表
       this.$store.dispatch('get_pw_user_table',this.current_group_role);   
       var _this = this;
+      //this.$refs.child调用子组件的方法
       var __this = this.$refs.child;
       //由于ajax是异步的，可能在执行这个方法前，值还没更新，所以使用延迟操作
       setTimeout(function(){
-        //深复制配置表，不然会影响到其他表
+        //深复制配置表，不然会影响到其他表(因为所有的表都会调用配置表)
         _this.copysys();
       },1000);
-      setTimeout(function(){     
+      setTimeout(function(){   
+        //初始化表单，下拉框和验证表,每一项生成属性并赋值空值
         __this.initialize_form();
+        //条件判断表赋值,让表单依据条件判断表生成内容
         __this.condition();
+        //生成select表值
+        __this.selectdrop();
+        //初始完毕之后让页面加载状态取消
         _this.loading = false;
-      },1500);
-    },
-    //切换
-    groupchange(){
-      //获取容器表
-      this.$store.dispatch('get_pw_user_table',this.current_group_role); 
-    },
-    //搜索
-    searchpw_user(){
-      this.$store.dispatch('searchpw_user_table',this.searchcontent);
+      },1500);     
     },
   },
   computed:{
@@ -169,15 +118,11 @@ export default {
     }
   },
   mounted(){
-    //初始化
+    //初始化加载内容
     this.loadingdata();
-    this.selectdrop();
   }
 }
 </script>
 <style scope>
-.el-select.inner .el-input{
-  width: 100px;
-  color: #cccccc;
-}
+
 </style>
